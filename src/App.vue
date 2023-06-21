2c5d11c6af5e41aeb798f5c142bc5fb322656ee6
Kenn Secusana <kennsecusana@gmail.com>
2023-06-21 09:09:56 +0800

    Pushed 13:43

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-
     <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container">
+          <RouterLink class="navbar-brand" to="/">Navbar</RouterLink>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink class="nav-link active" to="/">Home</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link active" to="/about"
+                  >About</RouterLink
+                >
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link active" to="/students"
+                  >Students</RouterLink
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   </header>
@@ -18,6 +45,4 @@ import HelloWorld from './components/HelloWorld.vue'
   <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/views/Students/View.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <main>
+      <h1>This is an Student Page</h1>
+    </main>
+  </template>
+  
+  
+  <style>
+  
+  </style>
+  
